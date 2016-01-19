6d4532ca3435f2c50f1af57181fa5752b6339c96
vicenteasd <vicenteasd@github.com>
2016-01-19 10:59:00 +0100

    feat: guarda el carrito en la base de datos
    Registra el total del carrito y el cliente en el servidorfeat: guarda el
    carrito en la base de datos
    
    Registra el total del carrito y el cliente en el servidor..

--- carrito.php
+++ carrito.php
@@ -2,7 +2,22 @@
   session_start();
   $carrito = $_POST["carrito"];
 
-  $obj_carrito = new stdClass();
+  $obj_carrito = new stdClass(); //Construyes obj
   $obj_carrito = json_decode($carrito);
+
  /*meter el carrito a la bd*/
+ header("Content-Type: text/html; charset=utf-8");
+ $link = mysql_connect("localhost", "root", "") or die ("No se pudo conectar a la BD" . mysql_error());
+
+ mysql_query("SET NAMES utf8");
+ mysql_select_db("shop") or die ("No se pudo seleccionar la base de datos");
+
+ //Cuando haya un sistema de usuarios funcional, hay que pasar el DNI del usuario en el carrito para ponerle el pedido a su nombre.
+ $SQL = 'INSERT INTO pedido(fecha,total,dni) VALUES (' . $obj_carrito->fecha . ',' . $obj_carrito->total . ',"52365874f")';
+ /*
+ for each de cada articulo en el carrito
+ "INSERT INTO linea_pedido(idPedido, idArticulo, unidad, precio, precioTotal) VALUES ()";*/
+ mysql_query($SQL) or die('Consulta fallida: ' . mysql_error());
+ mysql_close($link);
+ echo "as";
 ?>
