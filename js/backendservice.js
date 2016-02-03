09c625b9e667fd7dce9b185b79196568d302e52a
Jaime Manzanero <jaime.manza.ortiz@gmail.com>
2016-02-03 23:52:11 +0100

    CSS tienda/backend

--- js/backendservice.js
+++ js/backendservice.js
@@ -37,229 +37,6 @@ function validarEmail( email ) {
 
 }
 
-
-/* Categorias */
-function deleteCategoria(categoria){
-swal({
-  title: "¿Estás seguro de borrar la categoría?",
-  text: "¡No podrás recuperarla!",
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  cancelButtonText: "No",
-  confirmButtonText: "Sí",
-  closeOnConfirm: false
-},
-function(){
-  $.ajax({
-       url: './server/dao/categoriadao.php',
-       type: 'POST',
-       data: {'categoria':categoria},
-       success: function(data){
-       swal("¡Olé!",data,"success");
-      $('#jqGridCategorias').trigger( 'reloadGrid' );
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-});
- 
-}
-function abrirFormularioInsertCategoria(){
-  $('#modalCategoriaI').modal("show");
-  $('#insertarCategoriaBT').on('click',insertCategoria);
-}
-function insertCategoria(){
-  if ($('#nombreCategoriaTFI').val() == "" || $('#nombreCategoriaTFI').val() == null) {
-    swal("Rellena todos los campos.");
-  }else{
-    var categoria = new Object();
-
-    categoria.nombre = $('#nombreCategoriaTFI').val();
-    categoria.accion = "i";
-    var categoriaJson = JSON.stringify(categoria);
-
-
-      $.ajax({
-       url: './server/dao/categoriadao.php',
-       type: 'POST',
-       data: {'categoria':categoriaJson},
-       success: function(data){
-       swal("¡Olé!",data,"success");
-       $('#jqGridCategorias').trigger( 'reloadGrid' );
-       $('#modalCategoriaI').modal("hide");
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-  }
-}
-
-function abrirFormularioUpdateCategoria(){
-  $('#modalCategoriaU').modal("show");
-  $('#actualizarCategoriaBT').on('click',updateCategoria);
-}
-
-function updateCategoria(){
-  var categoria = new Object();
-  categoria.nombre = $('#nombreCategoriaTFU').val();
-  categoria.idCategoria = $(this).attr('idCategoria');
-  categoria.accion = "a";
-  var categoriaJson = JSON.stringify(categoria);
-
-  $.ajax({
-       url: './server/dao/categoriadao.php',
-       type: 'POST',
-       data: {'categoria':categoriaJson},
-       success: function(data){
-          swal("¡Olé!",data,"success");
-          $('#jqGridCategorias').trigger( 'reloadGrid' );
-          $('#modalCategoriaU').modal("hide");
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-
-}
-
-/* Articulos */
-
-function deleteArticulo(articulo){
-  swal({
-  title: "¿Estás seguro de borrar el artículo?",
-  text: "¡No podrás recuperarlo!",
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  cancelButtonText: "No",
-  confirmButtonText: "Sí",
-  closeOnConfirm: false
-},
-function(){
- $.ajax({
-         url: './server/dao/articulodao.php',
-       type: 'POST',
-       data: {'articulo':articulo},
-       success: function(data){
-       swal("¡Olé!",data,"success");
-      $('#jqGridArticulos').trigger('reloadGrid');
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-});
-  
-}
-
-
-function abrirFormularioInsertArticulo(){
-  if (!document.getElementById("categoriaSLI").options.length > 0) {
-      $.ajax({
-       url: './server/categorias.php',
-       dataType: 'json',
-       success: function(data){
-         $.each(data, function(){
-          $('#categoriaSLI').append("<option value='" + this.id + "'>" + this.nombre +"</option>");
-         });
-       }
-     });
-  };
-
-  $('#modalArticuloI').modal("show");
-  $('#insertarArticuloBT').on('click',insertArticulo);
-}
-
-function insertArticulo(){
- var valid = true;
- if ($('#nombreArticuloTFI').val() == "") {valid = false;};
- if ($('#descripcionArticuloTFI').val() == "") {valid = false;};
- if ($('#precioArticuloTFI').val() == "") {valid = false;};
- if ($('#stockArticuloTFI').val() == "") {valid = false;};
- if ($('#imagenI').val() == "") {valid = false;};
-
- var nombre = $('#nombreArticuloTFI').val();
- var descripcion = $('#descripcionArticuloTFI').val();
- var precio = $('#precioArticuloTFI').val();
- var stock = $('#stockArticuloTFI').val();
- var imagen = $('#imagenI').val();
- var categoria = document.getElementById('categoriaSLI').options[document.getElementById('categoriaSLI').selectedIndex].value;
-  if (!valid) {swal("Rellena todos los campos.");}else{
-
-    var articulo = new Object();
-    articulo.nombre = nombre;
-    articulo.descripcion = descripcion;
-    articulo.precio = precio;
-    articulo.stock = stock;
-    articulo.imagen = imagen;
-    articulo.categoria = categoria;
-    articulo.accion = "i";
-
-    var articuloJson = JSON.stringify(articulo);
-    $.ajax({
-       url: './server/dao/articulodao.php',
-       type: 'POST',
-       data: {'articulo':articuloJson},
-       success: function(data){
-           swal("¡Olé!",data,"success");
-          $('#jqGridArticulos').trigger('reloadGrid');
-          $('#modalArticuloI').modal("hide");
-
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-  }
-}
-
-function abrirFormularioUpdateArticulo(){
-  $('#modalArticuloU').modal('show');
-  $('#actualizarArticuloBT').on('click',updateArticulo);
-}
-
-function updateArticulo(){
-  var valid = true;
- if ($('#nombreArticuloTFU').val() == "") {valid = false;};
- if ($('#descripcionArticuloTFU').val() == "") {valid = false;};
- if ($('#precioArticuloTFU').val() == "") {valid = false;};
- if ($('#stockArticuloTFU').val() == "") {valid = false;};
- if ($('#imagenU').val() == "") {valid = false;};
- if (!valid) {
-  swal("Rellena todos los campos.");
- }else{
-  var articulo = new Object();
-  articulo.accion = "a";
-  articulo.idArticulo =  $(this).attr('idArticulo');
-  articulo.nombre = $('#nombreArticuloTFU').val();
-  articulo.descripcion = $('#descripcionArticuloTFU').val();
-  articulo.precio = $('#precioArticuloTFU').val();
-  articulo.stock = $('#stockArticuloTFU').val();
-  articulo.imagen = $('#imagenU').val();
-  articulo.categoria = document.getElementById('categoriaSLU').options[document.getElementById('categoriaSLU').selectedIndex].value;
-  var articuloJson = JSON.stringify(articulo);
-  $.ajax({
-       url: './server/dao/articulodao.php',
-       type: 'POST',
-       data: {'articulo':articuloJson},
-       success: function(data){
-          swal("¡Olé!",data,"success");
-          $("#jqGridArticulos").setGridParam({datatype: 'json',page:1}).trigger('reloadGrid');
-          $('#modalArticuloU').modal("hide");
-
-          },
-       error: function(data){
-          swal("¡Ups!",data.responseText,"error");
-       }
-     });
-
- }
-
-}
-
 /* Clientes  */
 
 function deleteCliente(cliente){
@@ -364,8 +141,16 @@ function insertCliente(){
        success: function(data){
          swal("¡Olé!",data,"success");
          $('#jqGridClientes').trigger('reloadGrid');
-         $('#modalClienteI').modal('hide');
+         /*vaciar campos*/
+         $('#nombreClienteTFI').val(null);
+         $('#apellidoClienteTFI').val(null);
+         $('#dniClienteTFI').val(null);
+         $('#direccionClienteTFI').val(null);
+         $('#telefonoClienteTFI').val(null);
+         $('#correoClienteTFI').val(null);
+         $('#contrasenyaClienteTFI').val(null);
 
+         $('#modalClienteI').modal('hide');
        },
        error: function(data){
         swal("¡Ups!",data.responseText,"error");
@@ -507,4 +292,226 @@ function cambiarContrasenyaCliente(){
        }
      });
   }
+}
+
+/* Categorias */
+function deleteCategoria(categoria){
+swal({
+  title: "¿Estás seguro de borrar la categoría?",
+  text: "¡No podrás recuperarla!",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  cancelButtonText: "No",
+  confirmButtonText: "Sí",
+  closeOnConfirm: false
+},
+function(){
+  $.ajax({
+       url: './server/dao/categoriadao.php',
+       type: 'POST',
+       data: {'categoria':categoria},
+       success: function(data){
+       swal("¡Olé!",data,"success");
+      $('#jqGridCategorias').trigger( 'reloadGrid' );
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+});
+
+}
+function abrirFormularioInsertCategoria(){
+  $('#modalCategoriaI').modal("show");
+  $('#insertarCategoriaBT').on('click',insertCategoria);
+}
+function insertCategoria(){
+  if ($('#nombreCategoriaTFI').val() == "" || $('#nombreCategoriaTFI').val() == null) {
+    swal("Rellena todos los campos.");
+  }else{
+    var categoria = new Object();
+
+    categoria.nombre = $('#nombreCategoriaTFI').val();
+    categoria.accion = "i";
+    var categoriaJson = JSON.stringify(categoria);
+
+
+      $.ajax({
+       url: './server/dao/categoriadao.php',
+       type: 'POST',
+       data: {'categoria':categoriaJson},
+       success: function(data){
+       swal("¡Olé!",data,"success");
+       $('#jqGridCategorias').trigger( 'reloadGrid' );
+       $('#modalCategoriaI').modal("hide");
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+  }
+}
+
+function abrirFormularioUpdateCategoria(){
+  $('#modalCategoriaU').modal("show");
+  $('#actualizarCategoriaBT').on('click',updateCategoria);
+}
+
+function updateCategoria(){
+  var categoria = new Object();
+  categoria.nombre = $('#nombreCategoriaTFU').val();
+  categoria.idCategoria = $(this).attr('idCategoria');
+  categoria.accion = "a";
+  var categoriaJson = JSON.stringify(categoria);
+
+  $.ajax({
+       url: './server/dao/categoriadao.php',
+       type: 'POST',
+       data: {'categoria':categoriaJson},
+       success: function(data){
+          swal("¡Olé!",data,"success");
+          $('#jqGridCategorias').trigger( 'reloadGrid' );
+          $('#modalCategoriaU').modal("hide");
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+
+}
+
+/* Articulos */
+
+function deleteArticulo(articulo){
+  swal({
+  title: "¿Estás seguro de borrar el artículo?",
+  text: "¡No podrás recuperarlo!",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  cancelButtonText: "No",
+  confirmButtonText: "Sí",
+  closeOnConfirm: false
+},
+function(){
+ $.ajax({
+         url: './server/dao/articulodao.php',
+       type: 'POST',
+       data: {'articulo':articulo},
+       success: function(data){
+       swal("¡Olé!",data,"success");
+      $('#jqGridArticulos').trigger('reloadGrid');
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+});
+
+}
+
+
+function abrirFormularioInsertArticulo(){
+  if (!document.getElementById("categoriaSLI").options.length > 0) {
+      $.ajax({
+       url: './server/categorias.php',
+       dataType: 'json',
+       success: function(data){
+         $.each(data, function(){
+          $('#categoriaSLI').append("<option value='" + this.id + "'>" + this.nombre +"</option>");
+         });
+       }
+     });
+  };
+
+  $('#modalArticuloI').modal("show");
+  $('#insertarArticuloBT').on('click',insertArticulo);
+}
+
+function insertArticulo(){
+ var valid = true;
+ if ($('#nombreArticuloTFI').val() == "") {valid = false;};
+ if ($('#descripcionArticuloTFI').val() == "") {valid = false;};
+ if ($('#precioArticuloTFI').val() == "") {valid = false;};
+ if ($('#stockArticuloTFI').val() == "") {valid = false;};
+ if ($('#imagenI').val() == "") {valid = false;};
+
+ var nombre = $('#nombreArticuloTFI').val();
+ var descripcion = $('#descripcionArticuloTFI').val();
+ var precio = $('#precioArticuloTFI').val();
+ var stock = $('#stockArticuloTFI').val();
+ var imagen = $('#imagenI').val();
+ var categoria = document.getElementById('categoriaSLI').options[document.getElementById('categoriaSLI').selectedIndex].value;
+  if (!valid) {swal("Rellena todos los campos.");}else{
+
+    var articulo = new Object();
+    articulo.nombre = nombre;
+    articulo.descripcion = descripcion;
+    articulo.precio = precio;
+    articulo.stock = stock;
+    articulo.imagen = imagen;
+    articulo.categoria = categoria;
+    articulo.accion = "i";
+
+    var articuloJson = JSON.stringify(articulo);
+    $.ajax({
+       url: './server/dao/articulodao.php',
+       type: 'POST',
+       data: {'articulo':articuloJson},
+       success: function(data){
+           swal("¡Olé!",data,"success");
+          $('#jqGridArticulos').trigger('reloadGrid');
+          $('#modalArticuloI').modal("hide");
+
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+  }
+}
+
+function abrirFormularioUpdateArticulo(){
+  $('#modalArticuloU').modal('show');
+  $('#actualizarArticuloBT').on('click',updateArticulo);
+}
+
+function updateArticulo(){
+  var valid = true;
+ if ($('#nombreArticuloTFU').val() == "") {valid = false;};
+ if ($('#descripcionArticuloTFU').val() == "") {valid = false;};
+ if ($('#precioArticuloTFU').val() == "") {valid = false;};
+ if ($('#stockArticuloTFU').val() == "") {valid = false;};
+ if ($('#imagenU').val() == "") {valid = false;};
+ if (!valid) {
+  swal("Rellena todos los campos.");
+ }else{
+  var articulo = new Object();
+  articulo.accion = "a";
+  articulo.idArticulo =  $(this).attr('idArticulo');
+  articulo.nombre = $('#nombreArticuloTFU').val();
+  articulo.descripcion = $('#descripcionArticuloTFU').val();
+  articulo.precio = $('#precioArticuloTFU').val();
+  articulo.stock = $('#stockArticuloTFU').val();
+  articulo.imagen = $('#imagenU').val();
+  articulo.categoria = document.getElementById('categoriaSLU').options[document.getElementById('categoriaSLU').selectedIndex].value;
+  var articuloJson = JSON.stringify(articulo);
+  $.ajax({
+       url: './server/dao/articulodao.php',
+       type: 'POST',
+       data: {'articulo':articuloJson},
+       success: function(data){
+          swal("¡Olé!",data,"success");
+          $("#jqGridArticulos").setGridParam({datatype: 'json',page:1}).trigger('reloadGrid');
+          $('#modalArticuloU').modal("hide");
+
+          },
+       error: function(data){
+          swal("¡Ups!",data.responseText,"error");
+       }
+     });
+
+ }
+
 }
