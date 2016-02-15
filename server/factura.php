df8bfef323fc43c3635264790bf68a3009e5d9b3
Jaime Manzanero <jaime.manza.ortiz@gmail.com>
2016-02-15 18:57:42 +0100

    Acabando generador PDF

--- server/factura.php
+++ server/factura.php
@@ -1,5 +1,6 @@
 <?php
 require('../fpdf/fpdf.php');
+define('EURO'," " . chr(128));
 
 class PDF extends FPDF
 {
@@ -24,10 +25,8 @@ class PDF extends FPDF
     $USUARIO = "SELECT c.nombre, c.apellido, p.fecha FROM cliente c, pedido p WHERE p.idPedido = ".$id." AND p.dni = c.dni;";
     $result_usuario = mysql_query($USUARIO) or die ('Consulta fallida: ' . mysql_error());
 
-
-    $width = array(50,30,40,40);
     //LOGO EcoRecipes
-    $this->Image('../img/logo.png',70,null,50,22);
+    $this->Image('../img/logo.png',70,null,70,25);
     $this->SetDrawColor(49,127,54);
     $this->SetFillColor(88,176,56);
 
@@ -36,16 +35,13 @@ class PDF extends FPDF
     $line = mysql_fetch_array($result_usuario, MYSQL_ASSOC);
     $nombreCompleto = $line['nombre'] . ' ' . $line['apellido'];
     $this->Cell(20,10,'Usuario: ');
-    $this->Cell(40,10,$nombreCompleto);
-    //$this->Cell(15,10,$line['nombre']);
-    //$this->Cell(20,10,$line['apellido']);
-    $this->Ln();
+    $this->Cell(80,10,$nombreCompleto);
     $this->Cell(20,10,'Fecha: ');
     $this->Cell(20,10,$line['fecha']);
-    $this->Ln();
-    $this->Ln();
+    $this->Ln(8);
 
     //TABLA
+    $width = array(70,30,50,40);
     $this->SetFont('Arial','B',14);
     for ($i=0; $i < count($header); $i++) {
       $this->Cell($width[$i],10,$header[$i],1,0,'C',true);
@@ -54,23 +50,30 @@ class PDF extends FPDF
     $this->SetFont('Arial','','11');
     $this->SetFillColor(132,210,143);
     $zebreado = false;
-    
+
+    $total = 0.0;
     while ($line = mysql_fetch_array($result, MYSQL_ASSOC)){
       $this->Cell($width[0],10,$line['nombre'],0,'LR','L',$zebreado);
-      $this->Cell($width[1],10,$line['unidad'],0,'LR','C',$zebreado);
-      $this->Cell($width[2],10,$line['precio'],0,'LR','C',$zebreado);
-      $this->Cell($width[3],10,$line['precioTotal'],0,'LR','C',$zebreado);
+      $this->Cell($width[1],10,number_format($line['unidad']),0,'LR','C',$zebreado);
+      $this->Cell($width[2],10,$line['precio'] . EURO,0,'LR','C',$zebreado);
+      $this->Cell($width[3],10,$line['precioTotal'] . EURO,0,'LR','C',$zebreado);
       $this->Ln();
       $zebreado = !$zebreado;
+      $total = $total + doubleval($line['precioTotal']);
     }
     //Linea de cierre
     $this->Cell(array_sum($width),0,'','T');
+    //$this->Ln();
+    $this->SetX(110);
+    $this->SetFont('Arial','B');
+    $this->Cell(50,10,'TOTAL:','RB',0,'C',$zebreado);
+    $this->Cell(40,10,$total . EURO,'LB',0,'C',$zebreado);
 
   }
 }
 
 $pdf = new PDF();
-//$pdf->SetFont('Arial','B',14);
+
 $pdf->AddPage();
 $header = array('Articulos', 'Unidades', 'Precio Unidades', 'Total Articulo');
 $pdf->BasicTable($header);
